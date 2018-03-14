4a5463628c29b2ac0d026c69288277b67af459f3
Will Szumski <will@stackhpc.com>
2018-03-14 17:12:53 +0000

    Add support for checking firmware version of network interface cards.
    A new HardwareManager was added which provides a verify_nic_firmware
    cleaning step. The list of cards to check is set through the nic_firmware
    property in the extra field of the Ironic node info.

---
+++ stackhpc_ipa_hardware_managers/system_nic.py
@@ -0,0 +1,241 @@
+# Copyright 2015 Rackspace, Inc.
+# Copyright 2018 StackHPC Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+import os
+
+from ironic_python_agent import errors
+from ironic_python_agent import hardware
+from ironic_python_agent import utils
+
+from oslo_concurrency import processutils
+from oslo_log import log
+
+UEVENT_PCI_SLOT_NAME_PREFIX = "PCI_SLOT_NAME="
+
+LOG = log.getLogger()
+
+
+def _get_lspci_output(vendor_id=None, device_id=None, **kwargs):
+    device_filter_arg = ''
+    if vendor_id or device_id:
+        device_filter_arg = "-d " + _get_device_filter_string(vendor_id,
+                                                              device_id)
+    lspci_output = ''
+    kwargs["shell"] = True
+    try:
+        lspci_output, _e = utils.execute(
+            "lspci -vmmnD {filter}".format(filter=device_filter_arg), **kwargs)
+    except (processutils.ProcessExecutionError, OSError) as e:
+        raise errors.CleaningError("Unable to run lspci")
+    return lspci_output
+
+
+def _parse_lspci_output(lspci_output):
+    all_records = lspci_output.split("\n\n")
+    devices = []
+    for record_str in all_records:
+        if record_str == "":
+            continue
+        fields_raw = record_str.splitlines()
+        fields = map((lambda x: x.split(':\t')), fields_raw)
+        devices.append(dict(list(map(tuple, fields))))
+    return devices
+
+
+def _get_device_filter_string(vendor_id, device_id):
+    device_id_str = ""
+    device_id_str += vendor_id or ""
+    device_id_str += ":"
+    device_id_str += device_id or ""
+    return device_id_str
+
+
+def _get_base_in_relative_path(path):
+    # given relative path, returns the base element, e.g one/two/three,
+    # returns one
+    if os.path.isabs(path):
+        raise ValueError("path must be relative")
+    dirname = os.path.dirname(path)
+    if dirname == "":
+        return path
+    else:
+        return _get_base_in_relative_path(dirname)
+
+
+def _pci_addr_to_net_interface(pci_addr):
+    for uevent_file in glob.glob("/sys/class/net/*/device/uevent"):
+        with open(uevent_file, 'r') as f:
+            for line in f.readlines():
+                if _is_matching_uevent_line(line, pci_addr):
+                    return _get_NIC_name(uevent_file)
+
+
+def _is_matching_uevent_line(line, pci_addr):
+    if line.startswith(UEVENT_PCI_SLOT_NAME_PREFIX):
+        # example : "PCI_SLOT_NAME=0000:00:19.0\n"
+        slot_name = line.lstrip(UEVENT_PCI_SLOT_NAME_PREFIX).strip()
+        if slot_name == pci_addr:
+            return True
+    return False
+
+
+def _get_NIC_name(uevent_file):
+    relative_path = os.path.relpath(uevent_file, '/sys/class/net/')
+    return _get_base_in_relative_path(relative_path)
+
+
+def _get_ethtool_output(dev, **kwargs):
+    kwargs["shell"] = True
+    ethtool_output = ''
+    try:
+        ethtool_output, _e = utils.execute(
+            "ethtool -i {device}".format(device=dev),
+            **kwargs)
+    except (processutils.ProcessExecutionError, OSError) as e:
+        raise errors.CleaningError("Unable to run ethtool")
+    return ethtool_output
+
+
+def _get_ethtool_field(ethtool_output, field):
+    for line in ethtool_output.splitlines():
+        (candidate_field, value) = tuple(line.split(": "))
+        if candidate_field == field:
+            return value
+
+
+def _get_expected_property(node, node_property):
+    try:
+        expected_property = node[node_property]
+    except KeyError as e:
+        raise errors.CleaningError(
+            "Expected property '{0}' not found. You should make sure all items"
+            " in the nic_firmware list contain {0}".format(node_property))
+    return expected_property
+
+
+def _get_nic_firmware_versions(vendor_id, device_id):
+    # there might be multiple identical cards, we must check them all
+    devices = _parse_lspci_output(_get_lspci_output(vendor_id, device_id))
+    interfaces = []
+    for device in devices:
+        interface_name = _pci_addr_to_net_interface(device['Slot'])
+        if interface_name is None:
+            raise errors.CleaningError(
+                "Could not determine network interface name. The pci_id was: "
+                "{vendor_id}:{device_id}. Does this correspond to a network "
+                "card?".format(vendor_id=vendor_id, device_id=device_id))
+        interfaces.append(interface_name)
+    return dict(map(lambda x: (
+        x, _get_ethtool_field(_get_ethtool_output(x), "firmware-version")),
+        interfaces))
+
+
+class SystemNICHardwareManager(hardware.HardwareManager):
+    """Checks firmware version for a given network card"""
+
+    HARDWARE_MANAGER_VERSION = '1.0'
+
+    def evaluate_hardware_support(self):
+        """Declare whether the system is supported by this manager.
+
+        :returns: HardwareSupport level for this manager.
+        """
+        # This should work for anything which supports ethtool, lspci
+        return hardware.HardwareSupport.SERVICE_PROVIDER
+
+    def get_clean_steps(self, node, ports):
+        """Get a list of clean steps with priority.
+
+        :param node: The node object as provided by Ironic.
+        :param ports: Port objects as provided by Ironic.
+        :returns: A list of cleaning steps, as a list of dicts.
+        """
+        return [{'step': 'verify_nic_firmware',
+                 'priority': 90,
+                 'interface': 'deploy',
+                 'reboot_requested': False,
+                 'abortable': True}]
+
+    def _get_lspci_output(*args, **kwargs):
+        return _get_lspci_output(*args, **kwargs)
+
+    def _pci_addr_to_net_interface(*args, **kwargs):
+        return _pci_addr_to_net_interface(*args, **kwargs)
+
+    def _get_ethtool_output(*args, **kwargs):
+        return _get_ethtool_output(*args, **kwargs)
+
+    def _get_nic_firmware_versions(self, vendor_id, device_id):
+        # there might be multiple identical cards, we must check them all
+        devices = _parse_lspci_output(
+            self._get_lspci_output(vendor_id, device_id))
+        interfaces = []
+        for device in devices:
+            interface_name = self._pci_addr_to_net_interface(device['Slot'])
+            if interface_name is None:
+                raise errors.CleaningError(
+                    "Could not network determine interface name. The pci_id "
+                    "was: {vendor_id}:{device_id}. Does this correspond to a "
+                    "network card?"
+                    .format(vendor_id=vendor_id, device_id=device_id))
+            interfaces.append(interface_name)
+        return dict(map(lambda x: (x, _get_ethtool_field(
+            self._get_ethtool_output(x), "firmware-version")), interfaces))
+
+    def verify_nic_firmware(self, node, ports):
+        """
+        Verify all firmware versions specified by the nic_firmware property.
+        """
+
+        if "extra" not in node or "nic_firmware" not in node["extra"]:
+            LOG.warning(
+                'NIC firmware property has not been set. No firmware will be '
+                'verified')
+            return True
+
+        firmwares = node["extra"]["nic_firmware"]
+
+        for firmware in firmwares:
+            self.process_firmware_descriptor(firmware)
+
+    def process_firmware_descriptor(self, firmware):
+        device_id = _get_expected_property(firmware, "device_id")
+        vendor_id = _get_expected_property(firmware, "vendor_id")
+        expected_version = _get_expected_property(firmware, "firmware_version")
+        interface_to_version_map = self._get_nic_firmware_versions(vendor_id,
+                                                                   device_id)
+        for interface_name, actual_version in interface_to_version_map.items():
+            if actual_version == expected_version:
+                LOG.debug("firmware version matches for interface: {}".format(
+                    interface_name))
+            else:
+                raise errors.CleaningError(
+                    "Firmware version mismatch for card: {interface}. The "
+                    "expected version was: {expected_version}, "
+                    "but the actual version was {actual_version}".format(
+                        interface=interface_name,
+                        expected_version=expected_version,
+                        actual_version=actual_version))
+
+# if __name__ == "__main__":
+# Access to some parts of the PCI configuration space is restricted to
+# root on many operating systems, so the features of lspci available to
+# normal users are limited
+
+# print(_get_lspci_output(None,None,run_as_root=True, root_helper="sudo"))
+# print repr(_get_ethtool_output("enp3s0"))
+# print repr(_get_lspci_output("10ec", None))
+# print _get_nic_firmware_versions("10EC", "8168")
---
+++ stackhpc_ipa_hardware_managers/tests/unit/test_system_nic.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2017 StackHPC Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import unittest
+
+from ironic_python_agent import errors
+from ironic_python_agent import hardware
+
+from stackhpc_ipa_hardware_managers import system_nic
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_dummy_node_info():
+    return {
+        'extra': {
+            'nic_firmware': [
+                {
+                    'vendor_id': '15B3',
+                    'device_id': '1013',
+                    'firmware_version': '12.20.1010'
+                }
+            ]
+        }
+    }
+
+
+def get_dummy_node_info_version_mismatch():
+    return {
+        'extra': {
+            'nic_firmware': [
+                {
+                    'vendor_id': '15B3',
+                    'device_id': '1013',
+                    'firmware_version': '1234.5678'
+                }
+            ]
+        }
+    }
+
+
+def get_lspci_output_multi():
+    with open(os.path.join(TEST_DIR, "data/lspci.output"), 'r') as f:
+        return f.read()
+
+
+def get_lspci_output_no_match():
+    return ""
+
+
+def get_lspci_output_one_match():
+    return \
+        'Slot:\t0000:03:00.0\nClass:\t0200\nVendor:\t10ec\nDevice:\t8168\n"' \
+        '"SVendor:\t1458\nSDevice:\te000\nRev:\t06\n\n'
+
+
+def get_ethtool_output():
+    with open(os.path.join(TEST_DIR, "data/ethtool.output"), 'r') as f:
+        return f.read()
+
+
+class TestSystemNIC(unittest.TestCase):
+
+    def test_parse_lspci_multi(self):
+        lspci_output = get_lspci_output_multi()
+        devices = system_nic._parse_lspci_output(lspci_output)
+        self.assertGreater(len(devices), 1)
+
+    def test_parse_lspci_no_match(self):
+        lspci_output = get_lspci_output_no_match()
+        devices = system_nic._parse_lspci_output(lspci_output)
+        self.assertEqual(len(devices), 0)
+
+    def test_parse_lspci_one_match(self):
+        lspci_output = get_lspci_output_one_match()
+        devices = system_nic._parse_lspci_output(lspci_output)
+        self.assertEqual(len(devices), 1)
+
+    def test_test_uevent_line_good(self):
+        input = "PCI_SLOT_NAME=0000:00:19.0"
+        result = system_nic._is_matching_uevent_line(input, "0000:00:19.0")
+        self.assertTrue(result)
+
+    def test_test_uevent_line_non_matching_addr(self):
+        input = "PCI_SLOT_NAME=0000:00:19.0"
+        result = system_nic._is_matching_uevent_line(input, "0000:00:18.0")
+        self.assertFalse(result)
+
+    def test_test_uevent_line_wrong_field_name(self):
+        input = "GARBAGE=0000:00:19.0"
+        result = system_nic._is_matching_uevent_line(input, "0000:00:19.0")
+        self.assertFalse(result)
+
+    def test_test_uevent_line_garbage(self):
+        input = "GARBAGE STRING"
+        result = system_nic._is_matching_uevent_line(input, "0000:00:19.0")
+        self.assertFalse(result)
+
+    def test_get_NIC_name(self):
+        input = "/sys/class/net/enp3s0/device/uevent"
+        result = system_nic._get_NIC_name(input)
+        self.assertEqual("enp3s0", result)
+
+    def test_get_ethtool_field(self):
+        input = get_ethtool_output()
+        result = system_nic._get_ethtool_field(input, "firmware-version")
+        self.assertEqual("12.20.1010", result)
+
+
+class SystemNICHardwareManagerMock(system_nic.SystemNICHardwareManager):
+    def _get_lspci_output(*args, **kwargs):
+        return get_lspci_output_one_match()
+
+    def _pci_addr_to_net_interface(*args, **kwargs):
+        return "enp3s0"
+
+    def _get_ethtool_output(*args, **kwargs):
+        return get_ethtool_output()
+
+
+class TestSystemNICManager(unittest.TestCase):
+
+    def setUp(self):
+        self.manager = SystemNICHardwareManagerMock()
+
+    def test_evaluate_hardware_support(self):
+        actual = self.manager.evaluate_hardware_support()
+        self.assertEqual(hardware.HardwareSupport.SERVICE_PROVIDER, actual)
+
+    def test_verify_nic_firmware(self):
+        # shouldn't throw
+        self.manager.verify_nic_firmware(get_dummy_node_info(), None)
+
+    def test_verify_nic_firmware_mismatch(self):
+        self.assertRaises(errors.CleaningError,
+                          self.manager.verify_nic_firmware,
+                          get_dummy_node_info_version_mismatch(), None)
